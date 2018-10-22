a6b332492e36bfc20f2a0da31b4bb02cfaa37c76
Bogdan <bbogdan2008@yahoo.com>
2018-10-23 01:10:11 +0300

    INT - remove warnings

--- client/src/actions/users.js
+++ client/src/actions/users.js
@@ -1,9 +1,23 @@
-import { USERS_LOGIN_REQUEST, USERS_LOGIN_SUCCES, USERS_LOGIN_FAILURE } from "../constants/action-types";
+import { USERS_LOGIN_REQUEST, USERS_LOGIN_SUCCESS, USERS_LOGIN_FAILURE } from "../constants/action-types";
 
 export const login = (email, pswd) => ({
   type: USERS_LOGIN_REQUEST,
   payload: {
     email: email, 
     pswd: pswd
+  }
+});
+
+export const loginSuccess = (token) => ({
+  type: USERS_LOGIN_SUCCESS,
+  payload: {
+    token: token
+  }
+});
+
+export const loginFailure = (err) => ({
+  type: USERS_LOGIN_FAILURE,
+  payload: {
+    error: err
   }
 });
