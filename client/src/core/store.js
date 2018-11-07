f21e3d209265bbf1cd422e03539c23c4fbb63198
Bogdan <bbogdan2008@yahoo.com>
2018-11-07 07:18:11 +0200

    INT - fix issue with redux-devtool

--- client/src/core/store.js
+++ client/src/core/store.js
@@ -10,12 +10,11 @@ import { planSaga } from "../plans/PlanSaga";
 // create the saga middleware
 const sagaMiddleware = createSagaMiddleware();
 
-const reduxDevTools =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
-  compose(applyMiddleware(sagaMiddleware), reduxDevTools)
+  composeEnhancers(applyMiddleware(sagaMiddleware))
 );
 
 // run the sagas
